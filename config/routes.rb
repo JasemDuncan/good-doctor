b98421db8858b55c992b31fbda8f43a9a7daf2e2
Jasem Duncan <jasemvalencia@gmail.com>
2022-02-11 11:33:08 -0500

    add react-router-dom

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
   root 'static#index'
 
   namespace :api do
@@ -8,4 +12,5 @@ Rails.application.routes.draw do
       get 'doctors', to: 'doctors#index'
     end
   end
+
 end
