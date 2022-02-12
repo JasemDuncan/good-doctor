0b39962d9b4c53fb558dfa5b9a412aea20eb2965
Cathella <ncathie214@gmail.com>
2022-02-12 21:17:57 +0300

    Add User scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :doctors
+  namespace :api do
+    namespace :v1 do
+      resources :doctors
+      
+      resources :users do
+        resources :appointments
+      end
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_12_163227) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_12_173227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
     t.date "appointmentDate"
     t.bigint "doctor_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -32,5 +34,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_12_163227) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "appointments", "doctors"
+  add_foreign_key "appointments", "users"
 end
