1503e2c73c4b85c4c9f44e6a930a99fbfeb03164
Cathella <ncathie214@gmail.com>
2022-02-21 12:30:59 +0300

    Update register component

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,36 +1,127 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { createUser } from '../redux/users/actions';
 import SideBar from '../components/SideBar';
 import './Register.css';
+class Register extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+      name: '',
+			username: '',
+			age: '',
+			password: '',
+			// picture: '',
+		};
+	}
 
-export default function Register() {
-  return (
+	handleNameChange = (e) => {
+		this.setState({
+			name: e.target.value,
+		});
+	}
+
+	handleUsernameChange = (e) => {
+		this.setState({
+			username: e.target.value,
+		});
+	}
+
+	handleAgeChange = (e) => {
+		this.setState({
+			age: e.target.value,
+		});
+	}
+
+	handlePasswordChange = (e) => {
+		this.setState({
+			password: e.target.value,
+		});
+	}
+
+	// handleAvatarChange = async (e) => {
+	// 	this.setState({
+	// 		picture: e.target.files[0],
+	// 	});
+	// }
+
+	// handleImageUpload = async (imageFile) => {
+	// 	const formData = new FormData();
+	// 	formData.append('file', imageFile);
+	// 	formData.append('upload_preset', 'avatar');
+	// 	delete axios.defaults.headers.common.Authorization;
+	// 	const response = await axios({
+	// 		url: 'your cloudinary api url',
+	// 		method: 'POST',
+	// 		data: formData,
+	// 	});
+	// 	return response.data.secure_url;
+	// }
+
+	onSubmitHandler = async (e) => {
+		e.preventDefault();
+		const {
+			name, username, age, password,
+		} = this.state;
+		// let { picture } = this.state;
+		const { newUser, user } = this.props;
+
+		// const imgPath = await this.handleImageUpload(picture);
+		// picture = imgPath;
+		await newUser({
+			name, username, age, password,
+		});
+		if (user.isLogin === true) {
+			const { history } = this.props;
+			history.push('/');
+		} else {
+			this.setState(
+				{
+					message: 'welcome',
+				},
+			);
+		}
+	}
+
+render() {
+  const { message } = this.state;
+	return (
     <>
       <SideBar />
-
       <div className="col-10">
-        <form>
-          <section className="row">
-            <div className="col-5 mx-auto">
-              <section className="bg-light mb-5 p-5">
-                <h2 className="mb-4 text-center">New User?</h2>
-                <div className="mb-3">
-                  <input name="fullName" className="form-control" placeholder="Your Name" type="text" value="" />
-                </div>
-                <div className="mb-3">
-                  <input name="username" className="form-control" placeholder="Username" type="text" value="" />
-                </div>
-                <div className="mb-3">
-                  <input name="age" className="form-control" placeholder="Your Age" type="number" value="" />
-                </div>
-                <div className="mb-3">
-                  <input name="password" className="form-control" placeholder="Password" type="password" value="" />
-                </div>
-                <button type="submit" className="btn btn-warning w-100">Register</button>
-              </section>
-            </div>
-          </section>
-        </form>
+        <div className="row mt-5 pt-5">
+          <form onSubmit={this.onSubmitHandler} className="col-5 shadow p-5 mx-auto bg-light mt-5">
+            <h2 className="mb-5">New User?</h2>
+            <input className="mb-3 form-control" onChange={this.handleNameChange} type="text" placeholder="Full name" required />
+            <input className="mb-3 form-control" onChange={this.handleUsernameChange} type="text" required placeholder="Username" />
+            <input className="mb-3 form-control" onChange={this.handleAgeChange} type="number" placeholder="Age" required />
+            <input className="mb-3 form-control" onChange={this.handlePasswordChange} type="password" placeholder="Password" required />
+            {/* <input onChange={this.handleAvatarChange} type="file" accept="/images/*" /> */}
+            <button className="btn btn-warning px-5" type="submit">Create Account</button>
+            <Link to="/login" className="ms-4">Login</Link>
+          </form>
+        </div>
       </div>
     </>
-  )
+		);
+	}
 }
+
+const mapStateToProps = state => ({
+	user: state.user,
+});
+
+const mapDispatchToProps = dispatch => ({
+	newUser: estate => dispatch(createUser(estate)),
+});
+
+Register.propTypes = {
+	newUser: PropTypes.func.isRequired,
+	user: PropTypes.object.isRequired,
+	history: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
