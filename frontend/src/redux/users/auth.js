598d0b31717da70da997d55fa48e73cb7f511db4
Cathella <ncathie214@gmail.com>
2022-02-21 11:48:35 +0300

    Add auth reducer and actions

---
+++ frontend/src/redux/users/auth.js
@@ -0,0 +1,28 @@
+const initialState = {
+	isLogin: false,
+	user: {
+		username: '',
+		email: '',
+		password: '',
+		picture: '',
+	}
+};
+const authReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'CREATE_USER': return {
+			...state.user,
+			isLogin: true,
+			user: {
+				username: action.username,
+				email: action.email,
+				password: action.password,
+				picture: action.picture,
+			},
+		};
+		case 'CREATE_USER_ERROR': return {
+			isLogin: false,
+		};
+		default: return state;
+	}
+};
+export default authReducer;
