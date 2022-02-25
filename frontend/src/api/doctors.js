24a70bdec02c686c0c9e5cb33a0d903fd9354686
Jasem Duncan <jasemvalencia@gmail.com>
2022-02-25 04:43:24 -0500

    remove front end for deployment

--- frontend/src/api/doctors.js
+++
@@ -1,5 +0,0 @@
-import http from '../services/httpService';
-import { BASE_URL } from '../utils/constants';
-
-export const getDoctorsAsync = async () => await http.get(`${BASE_URL}/doctors`);
-export const deleteDoctorAsync = async (doctorId) => await http.delete(`${BASE_URL}/doctors/${doctorId}`);
--- frontend/src/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8000/api/v1';
-export const AUTHORIZATION = 'Authorization';
-export const AUTH_TOKEN = 'auth_token';
